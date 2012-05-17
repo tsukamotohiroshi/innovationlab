34b8c7f39e42e8280ed6180e9436241b7ac645e9
tsukamoto <tsukamoto.hiroshi@connectstar.co.jp>
2012-05-17 19:36:29 +0900

    はいいいえを付けた

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 # myapp.rb
 require 'sinatra'
+require 'erb'
+
+post '/res' do
+  @name = params[:name]
+  @ans = params[:q1]
+
+  if @ans == "n"
+    @score = rand(500) + 500
+  elsif @ans == "y"
+    @score = rand(500)
+  else @score = 0  
+  end
+  
+  erb :result
+end
 
 get '/' do
-  'Hello world!'
+  erb :index
 end
